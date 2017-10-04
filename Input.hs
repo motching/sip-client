375e01fac4d451122572e111557841e2fa72b0ec
Attila Kovacs <akovacs101386@gmail.com>
2017-10-04 16:36:20 -0700

    Server code cleanup, succesfully recognizing INVITE via SIP over TCP

--- Input.hs
+++ Input.hs
@@ -1,6 +1,6 @@
 module Input where
 
---import Parser
+import Parser
 --import StateHandling
 import Types
 
@@ -8,26 +8,19 @@ import Network (listenOn, accept, PortID(..), Socket)
 import System.IO (hSetBuffering, hGetLine, hPutStrLn, BufferMode(..), Handle)
 import Control.Concurrent (forkIO)
 
---import Control.Concurrent
-
-sipMessage :: Connection -> IO String
-sipMessage = readFile --for now
-
 commandProcessor :: Handle -> IO ()
 commandProcessor handle = do
-    line <- hGetLine handle
-    let method = head $ words line
-    putStrLn "Itt vagyok, Jozsi"
-    putStrLn line
+    message <- hGetLine handle
+    let method = getRequestMethod message
+   -- let methodName = getMethodText method
     case method of
-        ("INVITE") -> hPutStrLn handle "INVITE received"
-        ("BYE") -> hPutStrLn handle "BYE received"
+        INVITE -> hPutStrLn handle "INVITE received"
+        BYE -> hPutStrLn handle "BYE received"
         _ -> hPutStrLn handle "Unknown command"
     commandProcessor handle
 
 sockHandler :: Socket -> IO ()
 sockHandler sock = do
-    putStrLn "sockHandler"
     (handle, _, _) <- accept sock --accept is a blocking call!!!
     hSetBuffering handle NoBuffering -- flush the buffer / discard content
     --_ <- ($) forkIO commandProcessor handle
@@ -40,14 +33,3 @@ sipListen state = do
     sock <- listenOn $ PortNumber port
     putStrLn $ "Listening on " ++ show port ++ " with state: " ++ show state
     sockHandler sock
-
--- sipListen :: State -> IO()
--- sipListen state = do
---   threadDelay 3000
---   message <- sipMessage "./message.bus"
---   let method = getRequestMethod message
---   let methodName = getMethodText method
---   putStrLn methodName
---   if newState method state
---     then putStrLn "yay, changed state!"
---     else sipListen state
