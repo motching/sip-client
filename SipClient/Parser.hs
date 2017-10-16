025caf711df3056ad828c11227d19eaaef131ccb
Attila Kovacs <akovacs101386@gmail.com>
2017-10-16 11:53:16 -0700

    Parse request-line

--- SipClient/Parser.hs
+++ SipClient/Parser.hs
@@ -3,26 +3,50 @@ module SipClient.Parser where
 import Prelude hiding (takeWhile)
 import SipClient.Types
 
-import Data.Word8
-import qualified Data.Attoparsec.ByteString as A
+import Data.Word8 hiding (isSpace)
+import Debug.Trace
+import qualified Data.Attoparsec.ByteString as AB
 import qualified Data.ByteString.Char8 as DBC
 
-parseOnly :: A.Parser a -> DBC.ByteString -> Either String a
-parseOnly = A.parseOnly
+parseOnly :: AB.Parser a -> DBC.ByteString -> Either String a
+parseOnly = AB.parseOnly
 
-parseSipMessage :: A.Parser SipMessage
+isEndOfLine :: Word8 -> Bool
+isEndOfLine w = w == 13 || w == 10   -- CR or LF
+
+isSpace :: Word8 -> Bool
+isSpace w = w == 32 || w == 9        --space or horizontal tab
+
+isColon :: Word8 -> Bool
+isColon w = w == 58                  -- colon
+
+skipSpace :: AB.Parser ()
+skipSpace = AB.skipWhile isSpace
+
+parseSipMessage :: AB.Parser SipMessage
 parseSipMessage = do
-    method <- parseReqMethod
-    callId <- parseCallId
-    return [(ReqMethod, method)
-           ,(CallId, callId)]
+    reqMethod <- parseReqMethod
+    uriScheme <- parseUriScheme
+    reqUri <- parseReqUri
+    sipVersion <- parseSipVersion
 
-parseReqMethod :: A.Parser DBC.ByteString
-parseReqMethod = A.takeTill isSpace
+    trace ("\nreqMethod: " ++ show reqMethod) return ()
+    trace ("\nuriScheme: " ++ show uriScheme) return ()
+    trace ("\nreqUri: " ++ show reqUri) return ()
+    trace ("\nsipVersion: " ++ show sipVersion) return ()
 
-newline :: A.Parser Word8
-newline = A.word8 10
+    --trace ("\ncallId: " ++ show callId) return ()
+    trace "\nall succeed!" return ()
+    return [(ReqMethod, reqMethod)]
 
-parseCallId :: A.Parser DBC.ByteString
-parseCallId = A.string (DBC.pack "Call-ID:") --why does $ not work here
-              *> A.takeTill (== 10) --newline - make constant
+parseReqMethod :: AB.Parser DBC.ByteString
+parseReqMethod = AB.takeTill isSpace <* AB.skipWhile isSpace
+
+parseUriScheme :: AB.Parser DBC.ByteString
+parseUriScheme = AB.takeTill isColon <* AB.skipWhile isColon
+
+parseReqUri :: AB.Parser DBC.ByteString
+parseReqUri = AB.takeTill isSpace <* AB.skipWhile isSpace
+
+parseSipVersion :: AB.Parser DBC.ByteString
+parseSipVersion = AB.takeTill isEndOfLine <* AB.skipWhile isEndOfLine
