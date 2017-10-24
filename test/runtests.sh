53cbfbbfc12f33f24a63135c48a3f7f39445885b
Attila Kovacs <akovacs101386@gmail.com>
2017-10-23 17:39:47 -0700

    UAS (orig side) testcase

--- test/runtests.sh
+++ test/runtests.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-#>results.txt
+rm testcases/*.log
+
 for FILE in testcases/*.xml
 do
   BN=$(basename $FILE)
-  echo Launching test $BN
-  sipp -trace_err -m 1 -sf $FILE -t u1 -l 1 127.0.0.1:1234
+  sipp -trace_err -trace_msg -m 1 -sf $FILE -t u1 -l 1 127.0.0.1:1234 &> /dev/null
   if test $? -ne 0
     then
-        echo -e "\e[31mTest $BN: FAIL\e[0m"
+        echo -e "\033[1;31mTest $BN: FAIL\033[0m"
     else
-        echo -e "\e[31mTest $BN: SUCCESS\e[0m"
+        echo -e "\033[1;32mTest $BN: SUCCESS\033[0m"
     fi
 done
+
 exit 0
+x
