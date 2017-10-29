0ba040111575e49da763524a881537f369eb4267
Attila Kovacs <akovacs101386@gmail.com>
2017-10-29 15:23:09 -0700

    Add some originating logic

--- SipClient/OrigLogic.hs
+++ SipClient/OrigLogic.hs
@@ -5,29 +5,35 @@ import SipClient.Types
 import qualified SipClient.UdpConnection as UDP
 import SipClient.UI
 
+import qualified Data.Bits as Bit
+import Data.Word
 import Control.Concurrent.STM
 import Network.Socket hiding (send, sendTo, recv, recvFrom)
 
 wait :: TVar UIData -> IO ()
 wait uiData = do
   command <- getChar
-  let res = case command of
-              'c' -> startCall uiData
-              _  -> wait uiData
-  res
-  return ()
+  case command of
+    'c' -> startCall uiData
+    _  -> wait uiData
 
 startCall :: TVar UIData -> IO ()
-startCall =  UDP.newSocket makeCall
+startCall tv = do
+    putStrLn $ "IP 2: " ++ show defaultRecipient
+    UDP.newSocket makeCall tv
+
+packIP :: Int -> Int -> Int -> Int -> Word32
+packIP a b c d = Bit.shift (fromIntegral d) 24
+                 + Bit.shift (fromIntegral c) 16
+                 + Bit.shift (fromIntegral b) 8
+                 + fromIntegral a
 
 defaultRecipient :: SockAddr
-defaultRecipient = SockAddrUnix "localhost"
+defaultRecipient = SockAddrInet 5060 (packIP 127 0 0 1)
 
 makeCall :: Socket -> TVar UIData -> IO ()
 makeCall sock uiData =  do
-  putStrLn "eeeee"
   let invite = return $ B.buildOutput B.newInvite
   _ <- UDP.sendMessages sock invite defaultRecipient
   refreshUI Orig INVITE uiData
-  --drawUI uiData
   return ()
--- SipClient/UI.hs
+++ SipClient/UI.hs
@@ -7,17 +7,32 @@ import Control.Concurrent.STM
 import System.Process
 
 refreshUI :: TransDirection -> ReqMethod -> TVar UIData -> IO ()
-refreshUI dir rm uiData = case rm of
-                        INVITE -> atomically $ modifyTVar uiData addInCall
-                        _ -> return ()
+refreshUI dir rm uiData =
+  case dir of
+    Term -> case rm of
+              INVITE -> atomically $ modifyTVar uiData addInCall
+              BYE -> atomically $ modifyTVar uiData removeInCall
+              _ -> return ()
+    Orig -> case rm of
+              INVITE -> atomically $ modifyTVar uiData addOutCall
+              BYE -> atomically $ modifyTVar uiData removeOutCall
+              _ -> return ()
 
 addInCall :: UIData -> UIData
-addInCall uid =  Data { numOfInCalls = 1 + numOfInCalls uid
+addInCall uid =  Data { numOfInCalls = numOfInCalls uid + 1
                       , numOfOutCalls = numOfOutCalls uid }
 
 addOutCall :: UIData -> UIData
 addOutCall uid =  Data { numOfInCalls = numOfInCalls uid
-                      , numOfOutCalls = 1 + numOfOutCalls uid }
+                      , numOfOutCalls = numOfOutCalls uid + 1}
+
+removeInCall :: UIData -> UIData
+removeInCall uid =  Data { numOfInCalls = numOfInCalls uid - 1
+                      , numOfOutCalls = numOfOutCalls uid }
+
+removeOutCall :: UIData -> UIData
+removeOutCall uid =  Data { numOfInCalls = numOfInCalls uid
+                      , numOfOutCalls = numOfOutCalls uid - 1}
 
 statusLine :: UIData -> String
 statusLine d = "Number of incoming calls: "
